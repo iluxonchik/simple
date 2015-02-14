4d7fe0f652584ea52d34b57f7bc539ad7b59eeb5
David Martins de Matos <david.m.matos@gmail.com>
2015-02-14 18:42:26 +0000

    LLVM is not supported in the IST version.

--- targets/llvm_ir_target.cpp
+++
@@ -1,5 +0,0 @@
-// $Id: llvm_ir_target.cpp,v 1.1 2014/05/02 22:33:16 david Exp $
-#include "targets/llvm_ir_target.h"
-
-/** @var create and register an evaluator for LLVM-IR targets. */
-simple::llvm_ir_target simple::llvm_ir_target::_self;
